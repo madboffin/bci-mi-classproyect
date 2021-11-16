6eac0b515d16c05f9f92e63bdc980a9076c28e9c
manuel_h <74844695+madboffin@users.noreply.github.com>
2021-11-16 12:33:17 -0500

    Added implementaion of CSP
    CSP example
    some funtions for edf

--- test.m
+++ test.m
@@ -1,29 +1,58 @@
-clear, clc
-load('Subject1_2D.mat')
-fs = 500;
-fc_low = 8;
-fc_high=30;
-f_order= 5;
-% 5,6,18 = c3,c4,cz
-tic
-[b,a]=butter(f_order,[fc_low,fc_high]/(fs/2),'bandpass');
-toc
-figure, freqz(b,a,fs,fs)
+clear, clc, close all
 
+% file specifics parameters
+load('Subject1_2D.mat')  % 5,6,18 = c3,c4,cz
+fs = 160;
+info = edfinfo('testrest.edf');
+c3rest = get_edfsignal('testrest.edf', {'C3..'});
+c4rest = get_edfsignal('testrest.edf', {'C4..'});
+czrest = get_edfsignal('testrest.edf', {'Cz..'});
+resting = [c3rest c4rest czrest];
 
-% [b,a]=butter(f_order,[fc_high]/(fs/2),'low');
-% figure, freqz(b,a,fs,fs)
-% 
-% [b,a]=butter(f_order,[fc_low]/(fs/2),'high');
-% figure, freqz(b,a,fs,fs)
+c3move = get_edfsignal('testright.edf', {'C3..'});
+c4move = get_edfsignal('testright.edf', {'C4..'});
+czmove = get_edfsignal('testright.edf', {'Cz..'});
+moving = [c3move c4move czmove];
 
-tic
-[b,a]=cheby2(f_order, 60,[fc_low,fc_high]/(fs/2),'bandpass');
-toc
+% general parameters
+mu = [ 8,12];
+be = [18,25];
+fc_low  =  8;
+fc_high = 30;
+f_order =  5;
+window  = 1*fs;
+overlap = 0.8*fs;
+[b,a] = butter(f_order,[fc_low,fc_high]/(fs/2),'bandpass');
 
-figure, freqz(b,a,fs,fs)
+% taking a portion of the signal (the size of a window)
+resting = resting(1:window,:);
+moving  =  moving(1:window,:);
+figure,plot(resting),title('original')
 
-filtsig=filtfilt(b,a,inputsig);  %filtered signal
+% filtering signal
+resting = filtfilt(b,a,resting);
+moving  = filtfilt(b,a, moving);
+figure,plot(resting),title('filtered')
 
-% function filt_signal[y]=(x)
-% end
+% csp filter (needs the 2 categories)[channels x samples]
+[W, lambda, A] = csp(moving',resting');
+restingX = (W'*resting')';
+movingX  = (W'* moving')';
+
+% relative power
+pMu_rest = bandpower(restingX,fs,mu)/bandpower(restingX);
+pBe_rest = bandpower(restingX,fs,be)/bandpower(restingX);
+
+pMu_move = bandpower(movingX,fs,mu)/bandpower(movingX);
+pBe_move = bandpower(movingX,fs,be)/bandpower(movingX);
+
+function [y] = filt_signal(b,a,x)
+    y = filtfilt(b,a,x);
+end
+
+function [signal] = get_edfsignal(edf_path, name_col)
+    original_signal = edfread(edf_path,'SelectedSignals', name_col);
+    original_signal = table2array(original_signal);
+    signal = vertcat(original_signal{:});
+end
+
