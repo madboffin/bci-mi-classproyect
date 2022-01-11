992a6d3de61500d76ee8ae4a5ec16ddb763f3f6b
manuel_h <74844695+madboffin@users.noreply.github.com>
2022-01-11 18:05:50 -0500

    Final update
    Create filter should receive sampling frequency

--- get_class.m
+++ get_class.m
@@ -1,5 +1,5 @@
 
-function predicted_class = main_classify(features)
+function predicted_class = get_class(features)
     load('Model_0 .mat','cl_lda')
     [~,ypredicted] = predict( cl_lda, features );
     [~,argmax] = max(ypredicted);
